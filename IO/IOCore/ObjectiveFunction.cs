8e6312aa44bf6fcee373eab2c16321130a64bdf0
diego llanos <diegojj1205@hotmail.com>
2017-10-20 18:47:35 -0400

    Restricciones  y algunas constantes de IO core

--- IO/IOCore/ObjectiveFunction.cs
+++ IO/IOCore/ObjectiveFunction.cs
@@ -8,26 +8,27 @@ using Math.Structures;
 
 namespace IOCore
 {
-    class ObjectiveFunction
+    public class ObjectiveFunction
     {
         Math.Structures.LinearEquation _FOEquation;
         Constants.Objetive _objetive;
-        ObjectiveFunction()
+        public ObjectiveFunction()
         {
             _FOEquation = new LinearEquation();
             _objetive = new Constants.Objetive();
         }
         public LinearEquation FOEquation { get => _FOEquation; set => _FOEquation = value; }
         internal Constants.Objetive Objetive { get => _objetive; set => _objetive = value; }
-        public Fraction SolutionZ(List<Fraction> _variants)
+        public void SolutionZ(List<Fraction> _variants)
         {
             int _tam = (_FOEquation.FirstTerms.Count),_counter=0;
-            Fraction Sol = new Fraction(1);
+            Fraction Sol = new Fraction(0);
             while (_counter<_tam)
             {
                 Sol += ((_FOEquation.FirstTerms[_counter])*(_variants[_counter]));
             }
-            return Sol;
+            _FOEquation.SecondTerm = Sol;
+            _FOEquation.Sign = Math.Constants.Signs.Equal;
         }//return Z value(entire problem solution)
     }
 }
--- IO/IOCore/Restriction.cs
+++ IO/IOCore/Restriction.cs
@@ -3,10 +3,78 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Math;
+using Math.Structures;
 
 namespace IOCore
 {
-    class Restriction
+    public class Restriction
     {
+        private Math.Structures.LinearEquation _resEquation;
+
+        public LinearEquation ResEquation { get => _resEquation; set => _resEquation = value; }
+
+        public Fraction ExcendentOrSlack(List<Fraction> _variants)
+        {
+            int _tam = _variants.Count;
+            int _counter = 0;
+            Fraction _sol=new Fraction(0);
+            Fraction _Resvariant=new Fraction(0);
+            while(_counter<_tam)
+            {
+                _sol += _variants[_counter] * _resEquation.FirstTerms[_counter];
+            }
+            if (_resEquation.Sign == Math.Constants.Signs.BiggerEqual || _resEquation.Sign == Math.Constants.Signs.Bigger)
+            {
+                return _resEquation.SecondTerm - _sol;
+            }
+            else if (_resEquation.Sign == Math.Constants.Signs.LessEqual || _resEquation.Sign == Math.Constants.Signs.Less)
+            {
+                return _sol - _resEquation.SecondTerm;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public bool AproovedRequeriment(List<Fraction> _variants)
+        {
+            int _tam = _variants.Count;
+            int _counter = 0;
+            Fraction _sol = new Fraction(0);
+            while (_counter < _tam)
+            {
+                _sol += _variants[_counter] * _resEquation.FirstTerms[_counter];
+            }
+            if(_resEquation.Sign==Math.Constants.Signs.BiggerEqual)
+            {
+                return (_sol >= _resEquation.SecondTerm);
+            }
+            else if(_resEquation.Sign == Math.Constants.Signs.Bigger)
+            {
+                return (_sol >_resEquation.SecondTerm);
+            }
+            else if(_resEquation.Sign == Math.Constants.Signs.LessEqual)
+            {
+                return (_sol <= _resEquation.SecondTerm);
+            }
+            else if(_resEquation.Sign == Math.Constants.Signs.Less)
+            {
+                return (_sol < _resEquation.SecondTerm);
+            }
+            else { return (_sol == _resEquation.SecondTerm); }
+            
+        }
+
+        public Fraction Term(int _pos)
+        {
+
+            return _resEquation.FirstTerms[_pos];
+        }
+        public Restriction()
+        {
+            _resEquation = new LinearEquation();
+        }
     }
 }
