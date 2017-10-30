ce26e94985747fd127dea019846cd889c02f5cd9
LeonardoADS <LeonardoADS08@gmail.com>
2017-10-30 17:47:48 -0400

    Cambios

--- IO/Utils/ErrorList.cs
+++ IO/Utils/ErrorList.cs
@@ -11,5 +11,6 @@ namespace Utils
         public static string Unknown = "Error #0: Unknown error.";
         public static string NotDivisbleByZero = "Error #1: No es posible la división entre cero.";
         public static string NotSquareMatrix = "Error #3: La matriz no es cuadrada, no es realizar la operación.";
+        public static string CantConvertToInt32 = "Error #4: No se puede convertir a entero.";
     }
 }
