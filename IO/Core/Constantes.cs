b435c830ece3e2bcf5207b413c9a2aa18678f141
LeonardoADS <LeonardoADS08@gmail.com>
2017-11-14 11:22:22 -0400

    14/11/17

---
+++ IO/Core/Constantes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public enum Signo
+    {
+        MayorQue = 1,
+        MayorIgualQue = 2,
+        MenorQue = 3,
+        MenorIgualQue = 4,
+        Igual = 5
+    }
+}
