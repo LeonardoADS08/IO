8de579e639645d1d8d4d03b098af0f24c89ce4a6
diego llanos <diegojj1205@hotmail.com>
2017-11-14 10:38:33 -0400

    :V

--- IO/Core/Simplex.cs
+++ IO/Core/Simplex.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.SolverFoundation.Services;
+using Microsoft.SolverFoundation.Common;
+using Microsoft.SolverFoundation.Solvers;
 
 namespace Core
 {
-    public class Simplex
+    class Simplex
     {
-        
-        public Simplex()
+
+        private Dictionary<string, double> _var;
+        private Dictionary<string,double>_restriction;
+        public Simplex(Dictionary<string, double> vars, Dictionary<string, double> restriction)
         {
-            
+            _var = vars;
+            _restriction = restriction;
         }
+
+
     }
 }
