2e8d199b6c08e4d4b492b30279c1c75ce0db215b
LeonardoADS <LeonardoADS08@gmail.com>
2017-11-30 19:04:52 -0400

    Terminando Modelo

--- IO/IO/Frames/Simplex/Modelo.xaml.cs
+++ IO/IO/Frames/Simplex/Modelo.xaml.cs
@@ -23,7 +23,7 @@ namespace IO.Frames.Simplex
     {
         public ObservableCollection<Core.MiembroFo> ListFO { get; set; }
         public ObservableCollection<Core.Restriction> ListRest { get; set; }
-
+        public ObservableCollection<dynamic> ListaSignos { get; set; }
         private int totalVariables, totalRestricciones;
         public Modelo(int restricciones, int variables)
         {
@@ -32,11 +32,40 @@ namespace IO.Frames.Simplex
             totalRestricciones = restricciones;
 
             ListFO = new ObservableCollection<Core.MiembroFo>();
+            ListRest = new ObservableCollection<Core.Restriction>();
+            ListaSignos = new ObservableCollection<dynamic>();
 
             DG_FO.ItemsSource = ListFO;
+            DG_Rest.ItemsSource = ListRest;
 
             for (int i = 0; i < variables; ++i)
                 ListFO.Add( new Core.MiembroFo() );
+            for (int i = 0; i < restricciones; ++i)
+            {
+                ListRest.Add(new Core.Restriction());
+                ListRest[i].Coef.Capacity = restricciones;
+            }
+
+            for (int i = 0; i < restricciones; ++i)
+            {
+                DataGridTextColumn columna = new DataGridTextColumn();
+                columna.Header = "Variable " + (i + 1).ToString();
+                columna.Binding = new Binding("Coef[" + i.ToString() + "]");
+                columna.Width = new DataGridLength(25, DataGridLengthUnitType.Star);
+                DG_Rest.Columns.Add(columna);
+            }
+
+
+            DataGridTextColumn columnaSigno = new DataGridTextColumn();
+            columnaSigno.Header = "Signo";
+            columnaSigno.Width = new DataGridLength(25, DataGridLengthUnitType.Star);
+            DG_Rest.Columns.Add(columnaSigno);
+
+            DataGridTextColumn ladoB = new DataGridTextColumn();
+            ladoB.Header = "Lado B";
+            ladoB.Width = new DataGridLength(25, DataGridLengthUnitType.Star);
+            DG_Rest.Columns.Add(ladoB);
+
 
         }
 
@@ -68,9 +97,83 @@ namespace IO.Frames.Simplex
                 ListFO.Add(new Core.MiembroFo());
         }
 
+        private void DG_FO_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            DependencyObject dep = (DependencyObject)e.EditingElement;
+            string newText = string.Empty;
+
+            if (dep is TextBox)
+            {
+                TextBox txt = dep as TextBox;
+                if (txt.Text != "") newText = txt.Text;
+                else newText = string.Empty;
+            }
+     
+            int columnIndex = -1, rowIndex = -1;
+            while ((dep != null) && !(dep is DataGridCell))
+                dep = VisualTreeHelper.GetParent(dep);
+
+            if (dep == null)  return;
+
+            if (dep is DataGridCell)
+            {
+                DataGridCell cell = dep as DataGridCell;
+                while ((dep != null) && !(dep is DataGridRow))
+                    dep = VisualTreeHelper.GetParent(dep);
+
+                if (dep == null) return;
+
+                DataGridRow row = dep as DataGridRow;
+                rowIndex = row.GetIndex();
+                columnIndex = cell.Column.DisplayIndex;
+            }
+
+            if (columnIndex == 0 && rowIndex != -1 && newText != "")
+                DG_Rest.Columns[rowIndex].Header = newText;
+        }
+
+        private void DG_Rest_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            DependencyObject dep = (DependencyObject)e.EditingElement;
+            string newText = string.Empty;
+
+            if (dep is TextBox)
+            {
+                TextBox txt = dep as TextBox;
+                if (txt.Text != "") newText = txt.Text;
+                else newText = string.Empty;
+            }
+
+            int columnIndex = -1, rowIndex = -1;
+            while ((dep != null) && !(dep is DataGridCell))
+                dep = VisualTreeHelper.GetParent(dep);
+
+            if (dep == null) return;
+
+            if (dep is DataGridCell)
+            {
+                DataGridCell cell = dep as DataGridCell;
+                while ((dep != null) && !(dep is DataGridRow))
+                    dep = VisualTreeHelper.GetParent(dep);
+
+                if (dep == null) return;
+
+                DataGridRow row = dep as DataGridRow;
+                rowIndex = row.GetIndex();
+                columnIndex = cell.Column.DisplayIndex;
+            }
+
+            // Indice de 'Signo'
+            if (columnIndex == totalRestricciones)
+            {
+                    if (Signos.signos.)
+            }
+        }
+
         private void DG_Rest_LoadingRow(object sender, DataGridRowEventArgs e)
         {
             e.Row.Header = "Restricción " + (e.Row.GetIndex() + 1).ToString() + ":";
+            e.Row.Height = 25;
         }
 
 
---
+++ IO/IO/Frames/Simplex/Signos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core;
+
+namespace IO.Frames.Simplex
+{
+    public static class Signos
+    {
+        public static Dictionary<string, Core.Signo> SignosDictionary = new Dictionary<string, Core.Signo>();
+
+        static Signos()
+        {
+            SignosDictionary.Add(">", Signo.MayorQue);
+            SignosDictionary.Add(">=", Signo.MayorIgualQue);
+            SignosDictionary.Add("=", Signo.Igual);
+            SignosDictionary.Add("<=", Signo.MenorIgualQue);
+            SignosDictionary.Add("<", Signo.MenorQue);
+        }
+    }
+}
